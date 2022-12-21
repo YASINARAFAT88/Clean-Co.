9f43d7e0327fda77df3cdfa52ffa733fc59da919
YASINARAFAT8 <yasin80545859@gmail.com>
2022-12-21 12:28:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
+import Navber from './Pages/Shared/Navber';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
+      <Navber></Navber>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Services from './Services/Services';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <Services></Services>
+            <Banner></Banner>
         </div>
     );
 };
